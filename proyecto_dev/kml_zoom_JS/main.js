21951eecd7347a330a1893e5e911e58a01f0cc6e
DaielChom <danielcarvajalpatino@gmail.com>
2018-02-16 09:11:49 -0500

    Busqueda KML estratos y Software para la recolccion de las imagenes

--- proyecto_dev/kml_zoom_JS/main.js
+++ proyecto_dev/kml_zoom_JS/main.js
@@ -9,7 +9,7 @@ function initMap() {
   var ctaLayer = new google.maps.KmlLayer({
     //var src = 'http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/educacion/bienestarestudiantil/MapServer/0/query?where=1=1&outFields=*&f=kmz';
 
-    url: 'http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/educacion/bienestarestudiantil/MapServer/0/query?where=1=1&outFields=*&f=kmz',
+    url: 'http://sinupotp.sdp.gov.co:6080/arcgis/rest/services/SERVICIOS_GEOGRAFICOS/ESTRATIFICACION/MapServer/4/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=1&geometry={"xmin":-8249695.5897576865,"ymin":518548.8015347421,"xmax":-8249084.093531458,"ymax":519160.2977609709,"spatialReference":{"wkid":102100,"latestWkid":3857}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100&f=kmz',
     suppressInfoWindows: true,
                 map: map,
                 preserveViewport: true
