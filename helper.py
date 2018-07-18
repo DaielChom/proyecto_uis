0a0234a98c3a17835bc8ae241a21b14928346cca
Daniel C Patiño <danielcarvajalpatino@gmail.com>
2018-07-18 16:07:46 -0500

    :pencil: new results presentation

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 def get_target_matrix(layer_to_extract):
@@ -72,3 +73,30 @@ def target_matrix_to_image_color(mask):
         if i == 6:
             newmat.append([2, 128, 2])
     return np.array(newmat).reshape((256,256,3))
+
+# Function create by soply on GithubGist
+def show_images(images, cols = 1, titles = None):
+    """Display a list of images in a single figure with matplotlib.
+    
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+plt.show()
